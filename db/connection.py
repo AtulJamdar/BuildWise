380e2b2e3707cf3ff5c46ad15fd43447d5da59b3
AtulJamdar <work.atuljamdar@gmail.com>
2026-07-12 20:03:17 +0530

    Databse_URL added

--- db/connection.py
+++ db/connection.py
@@ -4,11 +4,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 def get_connection():
+    database_url = os.getenv("DATABASE_URL")
+
+    if database_url:
+        return psycopg2.connect(database_url)
+
     return psycopg2.connect(
         host=os.getenv("DB_HOST", "localhost"),
         database=os.getenv("DB_NAME", "buildwise"),
         user=os.getenv("DB_USER", "postgres"),
         password=os.getenv("DB_PASSWORD", "postgres"),
-        port=os.getenv("DB_PORT", "5432")
+        port=os.getenv("DB_PORT", "5432"),
     )
